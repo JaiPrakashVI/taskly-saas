4f2c0f61b9b95cc4dad3e55a392d284dcd5af7f2
JaiPrakashVI <jaiprakashviofficial@gmail.com>
2026-06-18 22:47:09 +0530

    chore: remove tracked backend/dist directory

--- backend/dist/modules/dashboard/dashboard.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dashboard_controller_1 = require("./dashboard.controller");
-const auth_1 = require("../../middleware/auth");
-const router = (0, express_1.Router)();
-// Retrieve all aggregated stats and dashboard widgets (requires active JWT)
-router.get('/', auth_1.authenticateToken, dashboard_controller_1.getDashboardData);
-exports.default = router;
